c8df579859ad25a4ba7f91ba00bb5805df9b926c
E-code804 <erikpfeffer5@gmail.com>
2024-01-20 19:55:33 -0500

    Added mobile responsiveness.

--- base/views.py
+++ base/views.py
@@ -84,7 +84,7 @@ def home(request):
         Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)
     )  # icontains for some match
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     # For activity feed, shows activity for specified q topic, if any.
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
@@ -238,3 +238,9 @@ def updateUser(request):
             form.save()
             return redirect("user-profile", pk=user.id)
     return render(request, "base/update-user.html", {"form": form})
+
+
+def topicsPage(request):
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, "base/topics.html", {"topics": topics})
