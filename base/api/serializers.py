6c0b11a5390a5f45aaa67c7d4d24c62c134113cb
E-code804 <erikpfeffer5@gmail.com>
2024-01-21 22:17:11 -0500

    Completed v1 of project.

---
+++ base/api/serializers.py
@@ -0,0 +1,9 @@
+# Classes that take a model and turn it into JSON.
+from rest_framework.serializers import ModelSerializer
+from base.models import Room
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = "__all__"
